e26a28d44c224384bf8ae3d66b3ac439d95e7750
joseey23 <josephmaruria@gmail.com>
2023-04-26 13:32:11 +0300

    Server URL updated

--- client/script.js
+++ client/script.js
@@ -74,7 +74,7 @@ const handleSubmit = async (e) => {
 
   //fetching data from server
 
-  const response = await fetch("https://localhost:5000", {
+  const response = await fetch("https://najua.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
